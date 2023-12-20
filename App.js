bdaff54aff0d91458737b1d912be77199b22f5c1
sam-klein1 <sklein27@binghamton.edu>
2023-12-19 19:04:57 -0500

    Added settings page

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Home from './src/screens/Home';
 import Lots from './src/screens/Lots';
+import Settings from './src/screens/Settings'
 
 const Tab = createBottomTabNavigator();
 
@@ -16,24 +17,30 @@ function MyTabs() {
     //weird bs for the icons
     <Tab.Navigator
       screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
+        tabBarIcon: ({ color }) => {
           let iconName;
 
           if (route.name === 'Home') {
             iconName = 'local-parking'; // Change this to the actual name of your home icon
-          } else if (route.name === 'Lots') {
+          } 
+          else if (route.name === 'Lots') {
             iconName = 'format-list-bulleted'; // Change this to the actual name of your settings icon
+          }
+          else if (route.name === 'Settings') {
+            iconName = 'settings'; // Change this to the actual name of your settings icon
           }
 
           // You can return any component here, not just icons
-          return <Icon name={iconName} size={size} color={color} />;
+          return <Icon name={iconName} size={30} color={color} />;
         },
+        tabBarShowLabel: false
       })}
     >
 
       {/* Actual tabs */}
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Lots" component={Lots} />
+      <Tab.Screen name="Settings" component={Settings} />
 
 
     </Tab.Navigator>
--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,9 +1,8 @@
 import { View, Text } from "react-native";
 
-export default function Lots() {
+export default function Settings() {
   return (
     <View className="flex-1">
-
 
     </View>
   );
